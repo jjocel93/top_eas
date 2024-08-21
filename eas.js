fe914b05f8d5162b927f97fd9934de9b031a96b1
My Name <jjocel93@gmail.com>
2024-08-21 22:51:00 +0200

    Select all 16 squres using QSA to use in a for loop. Create for loops for the array-like results for a hover effect.

--- eas.js
+++ eas.js
@@ -4,6 +4,19 @@ const container = document.querySelector('#container');
 for (let i = 1; i <= 16; i++) {
   const div = document.createElement('div');
   div.classList.add('squares');
-
   container.appendChild(div);
+}
+
+const hover = document.querySelectorAll('.squares');
+
+for (let i = 0; i < hover.length; i++) {
+  hover[i].addEventListener('mouseover', () => {
+    hover[i].style.backgroundColor = 'lime';
+  });
+}
+
+for (let i = 0; i < hover.length; i++) {
+  hover[i].addEventListener('mouseout', () => {
+    hover[i].style.backgroundColor = '';
+  });
 }
