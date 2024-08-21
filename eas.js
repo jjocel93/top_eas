9a891e74f3f24c80a303db92948a745afa9ba6e3
My Name <jjocel93@gmail.com>
2024-08-21 15:54:04 +0200

    Create 16 boxes using create element. Boxes will be used for grid.

--- eas.js
+++ eas.js
@@ -4,5 +4,7 @@ const container = document.querySelector('#container');
 for (let i = 1; i <= 16; i++) {
   const div = document.createElement('div');
   div.classList.add('squares');
+  div.style.cssText =
+    'width: 100px; height: 100px; border-style: solid; border-color: black; border-width: 1px';
   container.appendChild(div);
 }
