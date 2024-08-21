e4b8b4b17bfc0be942805b1b225a90d0d45a4d57
My Name <jjocel93@gmail.com>
2024-08-21 15:41:38 +0200

    Create html, css, and js docs. Create main container in HTML. Select container div and create 16 children divs in js.

---
+++ eas.js
@@ -0,0 +1,8 @@
+//select the #container div
+const container = document.querySelector('#container');
+
+for (let i = 1; i <= 16; i++) {
+  const div = document.createElement('div');
+  div.classList.add('squares');
+  container.appendChild(div);
+}
