c6aa9a738758a43b0ca56e63cad73b8e5b1aa35d
My Name <jjocel93@gmail.com>
2024-08-22 18:34:20 +0200

    reset css and js.

--- eas.js
+++ eas.js
@@ -1,14 +1,18 @@
 //select the #container div
 const container = document.querySelector('#container');
 
-const squarecontain = document.createElement('div');
-squarecontain.classList.add('squarecontain');
-container.appendChild(squarecontain);
+const mainContainer = document.querySelector('#mainContainer');
 
 for (let i = 1; i <= 4; i++) {
-  const div = document.createElement('div');
-  div.classList.add('squares');
-  squarecontain.appendChild(div);
+  const squarecontain = document.createElement('div');
+  squarecontain.classList.add('squarecontain');
+  container.appendChild(squarecontain);
+
+  for (let i = 1; i <= 4; i++) {
+    const div = document.createElement('div');
+    div.classList.add('squares');
+    squarecontain.appendChild(div);
+  }
 }
 
 const hover = document.querySelectorAll('.squares');
@@ -28,25 +32,42 @@ for (let i = 0; i < hover.length; i++) {
 const button = document.createElement('button');
 button.textContent = 'Click me!';
 button.classList.add('button');
-container.insertAdjacentElement('afterbegin', button);
-
-// button.addEventListener('click', () => {
-//   const ans = prompt('Enter the amount of squares per side you want (max 100)');
-//   const nSquares = parseInt(ans);
-// });
+mainContainer.insertAdjacentElement('afterbegin', button);
 
 button.addEventListener('click', () => {
-  while (squarecontain.hasChildNodes()) {
-    squarecontain.removeChild(squarecontain.firstChild);
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
   }
 });
 
 button.addEventListener('click', () => {
   const ans = prompt('Enter the amount of squares per side you want (max 100)');
   const nSquares = parseInt(ans);
+  console.log(nSquares);
+
   for (let i = 1; i <= nSquares; i++) {
-    const nSquares = document.createElement('div');
-    nSquares.classList.add('newsquares');
-    squarecontain.appendChild(nSquares);
+    const squarecontain = document.createElement('div');
+    squarecontain.classList.add('squarecontain');
+    container.appendChild(squarecontain);
+
+    for (let i = 1; i <= nSquares; i++) {
+      const div = document.createElement('div');
+      div.classList.add('squares');
+      squarecontain.appendChild(div);
+    }
+  }
+
+  const hover = document.querySelectorAll('.squares');
+
+  for (let i = 0; i < hover.length; i++) {
+    hover[i].addEventListener('mouseover', () => {
+      hover[i].style.backgroundColor = 'lime';
+    });
+  }
+
+  for (let i = 0; i < hover.length; i++) {
+    hover[i].addEventListener('mouseout', () => {
+      hover[i].style.backgroundColor = '';
+    });
   }
 });
